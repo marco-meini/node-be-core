3f7e400b1152a0a0292784fcb75261a32e5da781
Marco Meini <marco@marcomeini.it>
2023-01-12 16:35:35 +0100

    v1.0.0

--- enums.mjs
+++ enums.mjs
@@ -3,23 +3,11 @@ const HttpResponseStatus = {
   NO_CONTENT: 204,
   NOT_AUTHENTICATED: 401,
   NOT_AUTHORIZED: 403,
-  MISSING_PARAMS: 400,
+  BAD_PARAMS: 400,
   NOT_FOUND: 404,
   CONFLICT: 409,
   ENANCE_YOUR_CALM: 420,
   SERVER_ERROR: 500
 };
 
-const RedisDb = {
-  TOKEN_SECRETS: 0,
-  USER_TOKENS: 1,
-  TOKEN_TO_MODIFY: 2,
-  GUESTS_TOKENS: 3
-};
-
-const NotificationServices = {
-  GOOGLE: "GOOGLE",
-  APPLE: "APPLE"
-};
-
-export { HttpResponseStatus, RedisDb, NotificationServices };
+export { HttpResponseStatus };
---
+++ express-middlewares/session-middleware.mjs
@@ -0,0 +1,149 @@
+import { HttpResponseStatus } from "../enums.mjs";
+import { MongoClienManager } from "../lib/mongo-client-manager.mjs";
+import { SessionManager } from "../lib/session-manager.mjs";
+
+/**
+ * @typedef {{
+ * session?: import("../lib/session-manager.mjs").ISession,
+ * } & import("express").Request} SessionRequest
+ */
+
+class SessionMiddleware {
+  /**
+   * @type {SessionManager}
+   */
+  sessionManager;
+
+  /** @type {string} */
+  sessionHeaderName;
+
+  /**
+   *
+   * @param {string} sessionHeaderName
+   * @param {number} sessionExpiration
+   * @param {MongoClienManager} mongoClient
+   */
+  constructor(sessionHeaderName, sessionExpiration, mongoClient) {
+    this.sessionHeaderName = sessionHeaderName;
+    this.sessionManager = new SessionManager(sessionExpiration, mongoClient);
+  }
+
+  /**
+   *
+   * @param {SessionRequest} request
+   * @returns
+   */
+  getToken(request) {
+    let header = request.header(this.sessionHeaderName);
+    if (header.startsWith("Bearer")) {
+      let headerSplit = header.split(" ");
+      return headerSplit.length > 1 ? headerSplit[1] : "";
+    }
+    return "";
+  }
+
+  /**
+   *
+   * @param {SessionRequest} request
+   * @param {import("express").Response} response
+   * @param {string} newToken
+   */
+  updateResponseToken(request, response, newToken) {
+    response.setHeader(this.sessionHeaderName, newToken);
+  }
+
+  /**
+   *
+   * @returns {import("express").RequestHandler}
+   */
+  checkAuthentication() {
+    return async (
+      /**@type {SessionRequest} */
+      request,
+      response,
+      next
+    ) => {
+      try {
+        let token = this.getToken(request);
+        if (token) {
+          request.session = await this.sessionManager.getSession(token);
+          next();
+        } else {
+          response.setHeader("WWW-Authenticate", 'Bearer realm="youneed"');
+          response.sendStatus(HttpResponseStatus.NOT_AUTHENTICATED);
+        }
+      } catch (e) {
+        response.sendStatus(HttpResponseStatus.NOT_AUTHENTICATED);
+      }
+    };
+  }
+
+  /**
+   *
+   * @param {string} permission
+   * @returns {import("express").RequestHandler}
+   */
+  checkPermission(permission) {
+    return async (
+      /** @type {SessionRequest} */
+      request,
+      response,
+      next
+    ) => {
+      try {
+        let token = this.getToken(request);
+        if (token) {
+          let sessionData = await this.sessionManager.getSession(token);
+          if (sessionData.grants.indexOf(permission) < 0) {
+            response.sendStatus(HttpResponseStatus.NOT_AUTHORIZED);
+          } else {
+            request.session = sessionData;
+            next();
+          }
+        } else {
+          response.setHeader("WWW-Authenticate", 'Bearer realm="youneed"');
+          response.sendStatus(HttpResponseStatus.NOT_AUTHENTICATED);
+        }
+      } catch (e) {
+        response.sendStatus(HttpResponseStatus.NOT_AUTHENTICATED);
+      }
+    };
+  }
+
+  /**
+   *
+   * @param {string[]} permissions
+   * @returns {import("express").RequestHandler}
+   */
+  checkAtLeastOnePermission(permissions) {
+    return async (
+      /** @type {SessionRequest} */
+      request,
+      response,
+      next
+    ) => {
+      try {
+        let token = this.getToken(request);
+        let sessionData = await this.sessionManager.getSession(token);
+        let authorized = false;
+        permissions.forEach((permission) => {
+          if (sessionData.grants.indexOf(permission) >= 0) {
+            authorized = true;
+            return;
+          }
+        });
+        if (!authorized) {
+          response.sendStatus(HttpResponseStatus.NOT_AUTHORIZED);
+        } else {
+          request.session = sessionData;
+          next();
+        }
+      } catch (e) {
+        response.setHeader("WWW-Authenticate", 'Bearer realm="youneed"');
+        response.sendStatus(HttpResponseStatus.NOT_AUTHENTICATED);
+      }
+    };
+  }
+}
+
+export { SessionMiddleware };
